ab3d8b60cb1ac2c921678ef65cd5aea4156da408
nelsonpovoa <nelsonpovoa@gmail.com>
2024-07-30 18:47:11 +0100

    projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrincipalComponent } from "./principal/principal.component";
+import { Principal2Component } from "./principal2/principal2.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PrincipalComponent, Principal2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
